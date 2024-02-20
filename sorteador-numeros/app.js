68580429e08955ace8e049ae350416f7c5cce2f9
Phelipe Rodrigues <phelipe.r.siqueira@gmail.com>
2024-02-19 22:20:36 -0300

    .

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,15 @@
+function sortear (){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero ;
+    
+    for (let i = 0; i < quantidade ; i++){
+        numero = obterNumeroAleatorio(de, ate)
+   }
+}
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
